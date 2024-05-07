95d132956bb67b958bf5e355e0e3ffeff0e98b2a
veersen2001 <veerbahadursen@gmail.com>
2024-05-07 21:40:19 +0530

    commit

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'https://lms-backend-0t1r.onrender.com/api/v1/' //http://localhost:5000
+const BASE_URL = 'http://localhost:5000/api/v1/'
+// https://lms-backend-0t1r.onrender.com/api/v1/
 
 const axiosInstance = axios.create();
 
