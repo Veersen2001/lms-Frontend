1ae5db7c7612194908edac19ed120db56642ac7f
veersen2001 <veerbahadursen@gmail.com>
2024-05-09 23:21:25 +0530

    ml

--- client/src/Components/Auth/RequireAuth.jsx
+++ client/src/Components/Auth/RequireAuth.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 function RequireAuth({ allowedRoles }) {
     const { isLoggedIn, role } = useSelector((state) => state.auth);
-
-    return isLoggedIn && allowedRoles.find((myRole) => myRole == role) ? (
+    const location = useLocation();
+    return isLoggedIn && allowedRoles.find((myRole) => myRole === role) ? (
         <Outlet/>
     ) : isLoggedIn ? ( <Navigate to="/denied"/>) : (<Navigate to="login" />)
 }
